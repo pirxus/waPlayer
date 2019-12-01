6ba5a50fbdba7e9cc80748a9cadf2dc912fe65dc
Simon Sedlacek <saimon.sedlacek@gmail.com>
2019-12-01 18:44:48 +0100

    more reorganizations

--- src/view.py
+++ src/view.py
@@ -1,5 +1,5 @@
-
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QPushButton
 
 class View(QtWidgets.QMainWindow):
 
@@ -7,3 +7,11 @@ class View(QtWidgets.QMainWindow):
         super(View, self).__init__()
         self.view = uic.loadUi('player.ui', self)
         self.show()
+
+    def addControls(self):
+        self.button = QtWidgets.QPushButton(self)
+        self.button.setText('suh')
+        self.button.move(50, 40)
+        self.button.show()
+
+
